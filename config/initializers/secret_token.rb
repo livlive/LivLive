c943367d2ba866e0225116cc553fe744c3f3e6ef
Scott MoorHouse <scottmoorhouse@gmail.com>
2012-06-13 10:30:05 -0500

    Starting App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivLive::Application.config.secret_token = 'f2ca5f5cefa7b331cd7ab0c87a2dd842b6b9b8feab00de6d9d5955dac8313b1978b037d6981b71ec07bf16d15bb313a76525ae737c7f1b268661d4bfcaa97317'
